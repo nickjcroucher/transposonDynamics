35015931f78bc195cab5694f8579a7cc8e661476
ph-u <51484056+ph-u@users.noreply.github.com>
2026-07-08 15:54:04 +0100

    m bsub job submission

--- src/sangerHPC.sh
+++ src/sangerHPC.sh
@@ -7,18 +7,10 @@
 # arg: 0
 # date: 20260702
 
-# BSUB -G team377f
-# BSUB -o ../work/pj02-%J-%I.o
-# BSUB -e ../work/pj02-%J-%I.e
-# BSUB -q long
-# BSUB -M 4000
-# BSUB -R "select[mem>4000] rusage[mem=4000] span[hosts=1]"
-# BSUB -J "tdy[1-360]"
-
 PATH="/software/isg/private/wrappers/apptainer/1.4.0:$PATH"
 
 apptainer pull tdy.sif oras://ghcr.io/nickjcroucher/transposondynamics:latest
 
-apptainer run --bind ${PWD}/../data:/data,${PWD}/../raw:/raw tdy.sif bash simMaster.sh ../raw/input.csv ../raw/seed.csv ../raw/scenario.csv ${LSB_JOBINDEX}
+bsub < sangerHPC_child.sh
 
 exit
---
+++ src/sangerHPC_child.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+# author: ph-u
+# script: sangerHPC_child.sh
+# desc: HPC script for transposon model
+# in: bsub sangerHPC_child.sh
+# out: NA
+# arg: 0
+# date: 20260702
+
+# BSUB -G team377f
+# BSUB -o ../work/pj02-%J-%I.o
+# BSUB -e ../work/pj02-%J-%I.e
+# BSUB -q long
+# BSUB -M 4000
+# BSUB -R "select[mem>4000] rusage[mem=4000] span[hosts=1]"
+# BSUB -J "tdy[1-360]"
+
+PATH="/software/isg/private/wrappers/apptainer/1.4.0:$PATH"
+
+apptainer run --bind ${PWD}/../data:/data,${PWD}/../raw:/raw tdy.sif bash simMaster.sh ../raw/input.csv ../raw/seed.csv ../raw/scenario.csv ${LSB_JOBINDEX}
+
+exit
